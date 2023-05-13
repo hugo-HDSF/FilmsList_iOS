bd41d2a92e6f04d688866705882a267ff30e2cb1
hugo-HDSF <hugo.dasilva.filipe@gmail.com>
2023-05-13 14:44:06 +0200

    Initial Commit

---
+++ App_ios/App_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_iosApp.swift
+//  App_ios
+//
+//  Created by Hugo Da Silva on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
